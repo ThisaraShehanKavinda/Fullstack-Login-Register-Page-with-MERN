620d1719fb3e127bbc7524e82b0bd87328f69afe
Thisara Shehan Kavinda <138697372+ThisaraShehanKavinda@users.noreply.github.com>
2025-06-01 19:30:12 +0530

    set the cookie with jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const { hashPassword, comparePassword } = require('../helpers/auth')
+const jwt = require('jsonwebtoken');
 
 
 
@@ -68,7 +69,10 @@ try {
     //Check Password match
     const match = await comparePassword(password, user.password)
     if (match) {
-        res.json('password match')
+        jwt.sign({email: user.email, id : user._id, name : user.name}, process.env.JWT_SECRET , {}, (err, token)=>{
+            if (err) throw err;
+            res.cookie('token', token).json(user)
+        })
     }
     if (!match) {
         res.json({
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const {mongoose} = require('mongoose')
 const app = express();
+const cookieParser = require('cookie-parser')
 
 //database connection
 mongoose.connect(process.env.MONGO_URL)
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err)=>console.log('Database not connected',err))
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
 
 
 
