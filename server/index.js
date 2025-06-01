17d4c857ebcf1c3a21f1dfdc08fcff00a663867b
Thisara Shehan Kavinda <138697372+ThisaraShehanKavinda@users.noreply.github.com>
2025-06-01 15:55:41 +0530

    create folder structure

--- server/index.js
+++ server/index.js
@@ -3,3 +3,6 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 
 const app = express();
+
+const port = 8000;
+app.listen(port, ()=>console.log(`Server is running on port ${port}`))
