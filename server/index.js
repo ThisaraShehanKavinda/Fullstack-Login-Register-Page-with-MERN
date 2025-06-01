e7101b9ca0feea3548ec0f38bc82c835013d5773
Thisara Shehan Kavinda <138697372+ThisaraShehanKavinda@users.noreply.github.com>
2025-06-01 16:46:05 +0530

    connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
 const cors = require('cors')
+const {mongoose} = require('mongoose')
+
+
+//database connection
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log('Database Connected'))
+.catch((err)=>console.log('Database not connected',err))
+
+
 
 const app = express();
 
